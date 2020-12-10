a9a0ae41e80d34b82ce73ae254595b27d8e8d882
Sam Garson <samtgarson@gmail.com>
2020-12-10 14:50:11 +0000

    Implement Database entities, response objects and factories

--- EvidenceApi.Tests/V1/Factories/ResponseFactoryTest.cs
+++ EvidenceApi.Tests/V1/Factories/ResponseFactoryTest.cs
@@ -1,19 +1,59 @@
+using System;
+using System.Collections.Generic;
+using AutoFixture;
+using EvidenceApi.V1.Boundary.Response;
 using EvidenceApi.V1.Domain;
 using EvidenceApi.V1.Factories;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace EvidenceApi.Tests.V1.Factories
 {
     public class ResponseFactoryTest
     {
-        //TODO: add assertions for all the fields being mapped in `ResponseFactory.ToResponse()`. Also be sure to add test cases for
-        // any edge cases that might exist.
+        private readonly IFixture _fixture = new Fixture();
+
         [Test]
-        public void CanMapADatabaseEntityToADomainObject()
+        public void CanMapAnEvidenceRequestDomainObjectToAResponseObject()
         {
-            // var domain = new Entity();
-            // var response = domain.ToResponse();
-            //TODO: check here that all of the fields have been mapped correctly. i.e. response.fieldOne.Should().Be("one")
+            var documentType = new DocumentType() {Id = "passport", Title = "Passport"};
+
+            var documentTypes = new List<DocumentType> {new DocumentType() {Id = "passport", Title = "Passport"}};
+            var domain = _fixture.Build<EvidenceRequest>()
+                .With(x => x.DocumentTypes, documentTypes)
+                .With(x => x.DeliveryMethods,
+                    new List<EvidenceRequest.DeliveryMethod> {EvidenceRequest.DeliveryMethod.Email})
+                .Create();
+
+            var response = domain.ToResponse();
+
+            var resident = new ResidentResponse()
+            {
+                Name = domain.Resident.Name,
+                Email = domain.Resident.Email,
+                PhoneNumber = domain.Resident.PhoneNumber,
+                Id = domain.Resident.Id
+            };
+
+            response.ServiceRequestedBy.Should().Be(response.ServiceRequestedBy);
+            response.DocumentTypes.Should().ContainSingle(x => x == "passport");
+            response.DeliveryMethods.Should().ContainSingle(x => x == "EMAIL");
+            response.Resident.Should().BeEquivalentTo(resident);
+            response.Id.Should().Be(domain.Id);
+            response.CreatedAt.Should().Be(domain.CreatedAt);
+        }
+
+        [Test]
+        public void CanMapAResidentDomainObjectToAResponseObject()
+        {
+            var domain = _fixture.Create<Resident>();
+
+            var response = domain.ToResponse();
+
+            response.Id.Should().Be(domain.Id);
+            response.Name.Should().Be(domain.Name);
+            response.Email.Should().Be(domain.Email);
+            response.PhoneNumber.Should().Be(domain.PhoneNumber);
         }
     }
 }
--- EvidenceApi/V1/Domain/EvidenceRequest.cs
+++ EvidenceApi/V1/Domain/EvidenceRequest.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Collections.Generic;
 
 namespace EvidenceApi.V1.Domain
 {
     public class EvidenceRequest
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
         public Resident Resident { get; set; }
         public List<DeliveryMethod> DeliveryMethods { get; set; }
-        public string DocumentType { get; set; }
+        public List<DocumentType> DocumentTypes { get; set; }
         public string ServiceRequestedBy { get; set; }
 
         public enum DeliveryMethod
