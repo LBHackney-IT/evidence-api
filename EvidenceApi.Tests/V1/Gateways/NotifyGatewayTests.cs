48492adc200d752bfd5dd70a9dcc33136774bb37
Sam Garson <samtgarson@gmail.com>
2020-12-15 14:35:02 +0000

    Basic notify client integration

--- EvidenceApi.Tests/V1/Gateways/NotifyGatewayTests.cs
+++ EvidenceApi.Tests/V1/Gateways/NotifyGatewayTests.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Collections.Generic;
 using AutoFixture;
+using EvidenceApi.V1.Domain;
+using EvidenceApi.V1.Domain.Enums;
 using EvidenceApi.V1.Gateways;
 using Moq;
 using Notify.Interfaces;
+using Notify.Models.Responses;
 using NUnit.Framework;
 
 namespace EvidenceApi.Tests.V1.Gateways
@@ -19,10 +24,43 @@ namespace EvidenceApi.Tests.V1.Gateways
             _classUnderTest = new NotifyGateway(_notifyClient.Object);
         }
 
-        [Test]
-        public void CanSendAnEmail()
+        [TestCase(CommunicationReason.EvidenceRequest, DeliveryMethod.Email, "NOTIFY_TEMPLATE_EVIDENCE_REQUESTED_EMAIL")]
+        [TestCase(CommunicationReason.EvidenceRejected, DeliveryMethod.Email, "NOTIFY_TEMPLATE_EVIDENCE_REJECTED_EMAIL")]
+        [TestCase(CommunicationReason.Reminder, DeliveryMethod.Email, "NOTIFY_TEMPLATE_REMINDER_EMAIL")]
+        public void CanSendAnEmail(CommunicationReason reason, DeliveryMethod deliveryMethod, string envVar)
         {
-            // _notifyClient.Setup()
+            var resident = _fixture.Create<Resident>();
+            var expectedTemplateId = Environment.GetEnvironmentVariable(envVar);
+
+            var response = _fixture.Create<EmailNotificationResponse>();
+            _notifyClient
+                .Setup(x =>
+                    x.SendEmail(resident.Email, expectedTemplateId, null, null, null))
+                .Returns(response)
+                .Verifiable();
+
+            _classUnderTest.SendNotification(deliveryMethod, reason, resident);
+            _notifyClient.Verify();
         }
+
+        [TestCase(CommunicationReason.EvidenceRequest, DeliveryMethod.Sms, "NOTIFY_TEMPLATE_EVIDENCE_REQUESTED_SMS")]
+        [TestCase(CommunicationReason.EvidenceRejected, DeliveryMethod.Sms, "NOTIFY_TEMPLATE_EVIDENCE_REJECTED_SMS")]
+        [TestCase(CommunicationReason.Reminder, DeliveryMethod.Sms, "NOTIFY_TEMPLATE_REMINDER_SMS")]
+        public void CanSendAnSms(CommunicationReason reason, DeliveryMethod deliveryMethod, string envVar)
+        {
+            var resident = _fixture.Create<Resident>();
+            var expectedTemplateId = Environment.GetEnvironmentVariable(envVar);
+
+            var response = _fixture.Create<SmsNotificationResponse>();
+            _notifyClient
+                .Setup(x =>
+                    x.SendSms(resident.PhoneNumber, expectedTemplateId, null, null, null))
+                .Returns(response)
+                .Verifiable();
+
+            _classUnderTest.SendNotification(deliveryMethod, reason, resident);
+            _notifyClient.Verify();
+        }
+
     }
 }
--- EvidenceApi/V1/Gateways/NotifyGateway.cs
+++ EvidenceApi/V1/Gateways/NotifyGateway.cs
@@ -1,7 +1,8 @@
 using System;
-using EvidenceApi.V1.Infrastructure.Interfaces;
-using Notify.Client;
+using EvidenceApi.V1.Domain;
+using EvidenceApi.V1.Domain.Enums;
 using Notify.Interfaces;
+using Notify.Models.Responses;
 
 namespace EvidenceApi.V1.Gateways
 {
@@ -11,6 +12,45 @@ namespace EvidenceApi.V1.Gateways
         public NotifyGateway(INotificationClient client)
         {
             _client = client;
+        }
+
+        public void SendNotification(DeliveryMethod deliveryMethod, CommunicationReason reason, Resident resident)
+        {
+            var result = Deliver(deliveryMethod, reason, resident);
+        }
+
+        private NotificationResponse Deliver(DeliveryMethod deliveryMethod, CommunicationReason reason, Resident resident)
+        {
+            return deliveryMethod switch
+            {
+                DeliveryMethod.Email => _client.SendEmail(resident.Email, GetEmailTemplateId(reason), null, null, null),
+                DeliveryMethod.Sms => _client.SendSms(resident.PhoneNumber, GetSmsTemplateId(reason), null, null, null),
+                _ => throw new ArgumentOutOfRangeException(nameof(deliveryMethod), deliveryMethod, $"Delivery Method {deliveryMethod.ToString()} not recognised")
+
+            };
+        }
+
+        private static string GetSmsTemplateId(CommunicationReason reason)
+        {
+            return reason switch
+            {
+                CommunicationReason.Reminder => Environment.GetEnvironmentVariable("NOTIFY_TEMPLATE_REMINDER_SMS"),
+                CommunicationReason.EvidenceRejected => Environment.GetEnvironmentVariable("NOTIFY_TEMPLATE_EVIDENCE_REJECTED_SMS"),
+                CommunicationReason.EvidenceRequest => Environment.GetEnvironmentVariable("NOTIFY_TEMPLATE_EVIDENCE_REQUESTED_SMS"),
+                _ => throw new ArgumentOutOfRangeException(nameof(reason), reason, $"Communication Reason {reason.ToString()} not recognised")
+            };
+        }
+
+
+        private static string GetEmailTemplateId(CommunicationReason reason)
+        {
+            return reason switch
+            {
+                CommunicationReason.Reminder => Environment.GetEnvironmentVariable("NOTIFY_TEMPLATE_REMINDER_EMAIL"),
+                CommunicationReason.EvidenceRejected => Environment.GetEnvironmentVariable("NOTIFY_TEMPLATE_EVIDENCE_REJECTED_EMAIL"),
+                CommunicationReason.EvidenceRequest => Environment.GetEnvironmentVariable("NOTIFY_TEMPLATE_EVIDENCE_REQUESTED_EMAIL"),
+                _ => throw new ArgumentOutOfRangeException(nameof(reason), reason, $"Communication Reason {reason.ToString()} not recognised")
+            };
         }
     }
 }
