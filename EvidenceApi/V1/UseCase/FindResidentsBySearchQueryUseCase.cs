309ac81f65fed990a6579e3faf5b7539a05e3a19
Neil Mendum <nmendum@gmail.com>
2021-04-01 12:21:14 +0100

    DES-75: tidy ups

--- EvidenceApi/V1/UseCase/FindResidentsBySearchQueryUseCase.cs
+++ EvidenceApi/V1/UseCase/FindResidentsBySearchQueryUseCase.cs
@@ -21,40 +21,40 @@ namespace EvidenceApi.V1.UseCase
 
         public List<ResidentResponse> Execute(ResidentSearchQuery request)
         {
-            var results = new List<ResidentResponse>();
-            findByResidentDetails(request, results);
-            findByResidentReferenceId(request, results);
-            return results;
+            var residents = new List<ResidentResponse>();
+            findByResidentDetails(request, residents);
+            findByResidentReferenceId(request, residents);
+            return residents;
         }
 
-        private void findByResidentDetails(ResidentSearchQuery request, ICollection<ResidentResponse> results)
+        private void findByResidentDetails(ResidentSearchQuery request, ICollection<ResidentResponse> residents)
         {
-            var residentSearchByNameEmailPhoneNumber = _residentsGateway.FindResidents(request.SearchQuery);
+            var residentsForSearchQuery = _residentsGateway.FindResidents(request.SearchQuery);
 
-            foreach (var resident in residentSearchByNameEmailPhoneNumber)
+            foreach (var resident in residentsForSearchQuery)
             {
                 var evidenceRequestsForResident = _evidenceGateway.FindEvidenceRequestsByResidentId(resident.Id);
                 foreach (var evidenceRequest in evidenceRequestsForResident)
                 {
                     if (evidenceRequest.ServiceRequestedBy.Equals(request.ServiceRequestedBy))
                     {
-                        results.Add(resident.ToResponse(evidenceRequest.ResidentReferenceId));
+                        residents.Add(resident.ToResponse(evidenceRequest.ResidentReferenceId));
                         // if any of the evidence requests were created by ServiceRequestedBy from the query then this resident should be returned
-                        // we can then break out of the loop early
+                        // and we can then break out of the loop early.
                         break;
                     }
                 }
             }
         }
 
-        private void findByResidentReferenceId(ResidentSearchQuery request, ICollection<ResidentResponse> results)
+        private void findByResidentReferenceId(ResidentSearchQuery request, ICollection<ResidentResponse> residents)
         {
-            var evidenceRequestsByTeamAndResidentReferenceId = _evidenceGateway.GetEvidenceRequests(request);
-            if (evidenceRequestsByTeamAndResidentReferenceId.Count > 0)
+            var evidenceRequestsForTeamAndSearchQuery = _evidenceGateway.GetEvidenceRequests(request);
+            if (evidenceRequestsForTeamAndSearchQuery.Count > 0)
             {
-                var evidenceRequest = evidenceRequestsByTeamAndResidentReferenceId.First();
-                var residentByTeamAndResidentReferenceId = _residentsGateway.FindResident(evidenceRequest.ResidentId);
-                results.Add(residentByTeamAndResidentReferenceId.ToResponse(evidenceRequest.ResidentReferenceId));
+                var evidenceRequest = evidenceRequestsForTeamAndSearchQuery.First();
+                var residentForTeamAndSearchQuery = _residentsGateway.FindResident(evidenceRequest.ResidentId);
+                residents.Add(residentForTeamAndSearchQuery.ToResponse(evidenceRequest.ResidentReferenceId));
             }
         }
     }
