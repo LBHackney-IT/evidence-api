355aeeb2fbd5e681a8da85dd39ae535143bc3b0d
Bogdan <bogdan@madetech.com>
2021-04-01 12:38:57 +0100

    Refactor UpdateDocumentSubmissionStateUseCase

--- EvidenceApi/V1/UseCase/UpdateDocumentSubmissionStateUseCase.cs
+++ EvidenceApi/V1/UseCase/UpdateDocumentSubmissionStateUseCase.cs
@@ -61,46 +61,58 @@ namespace EvidenceApi.V1.UseCase
 
             if (!String.IsNullOrEmpty(request.RejectionReason) && documentSubmission.State == SubmissionState.Rejected)
             {
-                documentSubmission.RejectionReason = request.RejectionReason;
-                var evidenceRequest = _evidenceGateway.FindEvidenceRequest(documentSubmission.EvidenceRequestId);
-
-                if (evidenceRequest == null)
-                {
-                    throw new NotFoundException($"Cannot find evidence request with id: {documentSubmission.EvidenceRequestId}");
-                }
-
-                var resident = _residentsGateway.FindResident(evidenceRequest.ResidentId);
-
-                if (resident == null)
-                {
-                    throw new NotFoundException($"Cannot find resident with id: {evidenceRequest.ResidentId}");
-                }
-
-                try
-                {
-                    documentSubmission.EvidenceRequest.DeliveryMethods.ForEach(dm =>
-                        _notifyGateway.SendNotification(dm, CommunicationReason.EvidenceRejected, documentSubmission, resident));
-                }
-                catch (NotifyClientException ex)
-                {
-                    Console.Error.WriteLine(ex);
-                    throw new NotificationException(documentSubmission.EvidenceRequest);
-                }
+                NotifyResident(documentSubmission, request);
             }
 
             DocumentType staffSelectedDocumentType = null;
             if (!String.IsNullOrEmpty(request.StaffSelectedDocumentTypeId))
             {
-                documentSubmission.StaffSelectedDocumentTypeId = request.StaffSelectedDocumentTypeId;
-                var evidenceRequest = _evidenceGateway.FindEvidenceRequest(documentSubmission.EvidenceRequestId);
-                staffSelectedDocumentType = _staffSelectedDocumentTypeGateway.GetDocumentTypeByTeamNameAndDocumentTypeId(
-                    evidenceRequest.ServiceRequestedBy, request.StaffSelectedDocumentTypeId);
+                staffSelectedDocumentType = GetStaffSelectedDocumentType(documentSubmission, request);
             }
             _evidenceGateway.CreateDocumentSubmission(documentSubmission);
             _updateEvidenceRequestStateUseCase.Execute(documentSubmission.EvidenceRequestId);
 
             var documentType = _documentTypeGateway.GetDocumentTypeByTeamNameAndDocumentTypeId(documentSubmission.EvidenceRequest.ServiceRequestedBy, documentSubmission.DocumentTypeId);
             return documentSubmission.ToResponse(documentType, staffSelectedDocumentType);
+        }
+
+        private void NotifyResident(DocumentSubmission documentSubmission, DocumentSubmissionUpdateRequest request)
+        {
+            documentSubmission.RejectionReason = request.RejectionReason;
+            var evidenceRequest = _evidenceGateway.FindEvidenceRequest(documentSubmission.EvidenceRequestId);
+
+            if (evidenceRequest == null)
+            {
+                throw new NotFoundException($"Cannot find evidence request with id: {documentSubmission.EvidenceRequestId}");
+            }
+
+            var resident = _residentsGateway.FindResident(evidenceRequest.ResidentId);
+
+            if (resident == null)
+            {
+                throw new NotFoundException($"Cannot find resident with id: {evidenceRequest.ResidentId}");
+            }
+
+            try
+            {
+                documentSubmission.EvidenceRequest.DeliveryMethods.ForEach(dm =>
+                    _notifyGateway.SendNotification(dm, CommunicationReason.EvidenceRejected, documentSubmission, resident));
+            }
+            catch (NotifyClientException ex)
+            {
+                Console.Error.WriteLine(ex);
+                throw new NotificationException(documentSubmission.EvidenceRequest);
+            }
+        }
+
+        private DocumentType GetStaffSelectedDocumentType(DocumentSubmission documentSubmission, DocumentSubmissionUpdateRequest request)
+        {
+            DocumentType staffSelectedDocumentType = null;
+            documentSubmission.StaffSelectedDocumentTypeId = request.StaffSelectedDocumentTypeId;
+            var evidenceRequest = _evidenceGateway.FindEvidenceRequest(documentSubmission.EvidenceRequestId);
+            staffSelectedDocumentType = _staffSelectedDocumentTypeGateway.GetDocumentTypeByTeamNameAndDocumentTypeId(
+                evidenceRequest.ServiceRequestedBy, request.StaffSelectedDocumentTypeId);
+            return staffSelectedDocumentType;
         }
     }
 }
