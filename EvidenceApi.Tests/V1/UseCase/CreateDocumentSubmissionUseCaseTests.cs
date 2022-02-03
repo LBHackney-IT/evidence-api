f78a5e4cdf38a0923a02c77a3ab555a35ae7c1fa
Bella <arabella.cockrell@gmail.com>
2022-02-03 17:15:30 +0000

    Update Validator tests with specific error message checks

--- EvidenceApi.Tests/V1/UseCase/CreateDocumentSubmissionUseCaseTests.cs
+++ EvidenceApi.Tests/V1/UseCase/CreateDocumentSubmissionUseCaseTests.cs
@@ -44,7 +44,7 @@ namespace EvidenceApi.Tests.V1.UseCase
                 .Without(x => x.DocumentType)
                 .Create();
             Func<Task<DocumentSubmissionResponse>> testDelegate = async () => await _classUnderTest.ExecuteAsync(Guid.NewGuid(), documentSubmissionRequest).ConfigureAwait(true);
-            testDelegate.Should().Throw<BadRequestException>();
+            testDelegate.Should().Throw<BadRequestException>().WithMessage("Document type is null or empty");
         }
 
         [Test]
@@ -55,7 +55,7 @@ namespace EvidenceApi.Tests.V1.UseCase
                 .Create();
 
             Func<Task<DocumentSubmissionResponse>> testDelegate = async () => await _classUnderTest.ExecuteAsync(Guid.NewGuid(), documentSubmissionRequest).ConfigureAwait(true);
-            testDelegate.Should().Throw<BadRequestException>(); 
+            testDelegate.Should().Throw<BadRequestException>().WithMessage("Base 64 document is null or empty"); 
         }
 
         [Test]
@@ -66,7 +66,7 @@ namespace EvidenceApi.Tests.V1.UseCase
                 .Create();
 
             Func<Task<DocumentSubmissionResponse>> testDelegate = async () => await _classUnderTest.ExecuteAsync(Guid.NewGuid(), documentSubmissionRequest).ConfigureAwait(true);
-            testDelegate.Should().Throw<BadRequestException>(); 
+            testDelegate.Should().Throw<BadRequestException>().WithMessage("Base64 mime type is not accepted"); 
         }
 
         [Test]
