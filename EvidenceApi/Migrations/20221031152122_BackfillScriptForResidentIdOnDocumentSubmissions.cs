ee4fc20e6cacb191c1a1373c63be0cd69f218813
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2022-10-31 15:55:33 +0000

    migration to backfill resident_id on document_submissions (historic records), but better

--- EvidenceApi/Migrations/20221031152122_BackfillScriptForResidentIdOnDocumentSubmissions.cs
+++ EvidenceApi/Migrations/20221031152122_BackfillScriptForResidentIdOnDocumentSubmissions.cs
@@ -22,14 +22,35 @@ namespace EvidenceApi.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropForeignKey(
+                name: "FK_document_submissions_residents_resident_id",
+                table: "document_submissions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_document_submissions_resident_id",
+                table: "document_submissions");
+
             migrationBuilder.DropColumn(
                 name: "resident_id",
                 table: "document_submissions");
+
             migrationBuilder.AddColumn<Guid>(
                 name: "resident_id",
                 table: "document_submissions",
                 nullable: true);
 
+            migrationBuilder.CreateIndex(
+                name: "IX_document_submissions_resident_id",
+                table: "document_submissions",
+                column: "resident_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_document_submissions_residents_resident_id",
+                table: "document_submissions",
+                column: "resident_id",
+                principalTable: "residents",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
         }
     }
 }
