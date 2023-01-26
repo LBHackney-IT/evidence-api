aa50270f5a90d33a4a4f3bbd05d50da951a712c6
Charli Wild <charli.wild@charlis-mbp.lan>
2023-01-26 10:54:01 +0000

    added skeleton controller

---
+++ EvidenceApi/V1/Controllers/ClaimsBackfillController.cs
@@ -0,0 +1,30 @@
+using EvidenceApi.V1.UseCase.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvidenceApi.V1.Controllers;
+
+[ApiController]
+[Route("api/v1/claims/backfill")]
+[Produces("application/json")]
+[ApiVersion("1.0")]
+
+public class ClaimsBackfillController: BaseController
+{
+    public ClaimsBackfillController(ICreateAuditUseCase createAuditUseCase) : base(createAuditUseCase)
+    {
+    }
+
+    [HttpGet]
+    public IActionResult BackfillClaimsWithGroupId()
+    {
+        try
+        {
+            return Ok("Not yet implemented");
+        }
+        catch
+        {
+            return BadRequest("bad request");
+        }
+    }
+
+}
