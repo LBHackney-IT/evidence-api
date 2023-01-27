a21f505729528c9ed8737c2a9ddb40e012e9aca9
Charli Wild <charli.wild@charlis-mbp.lan>
2023-01-27 20:31:42 +0000

    fix timestamp

--- EvidenceApi/Startup.cs
+++ EvidenceApi/Startup.cs
@@ -137,6 +137,8 @@ namespace EvidenceApi
             var options = AppOptions.FromEnv();
             services.AddSingleton(x => options);
 
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
             // Database Context
             services.AddDbContext<EvidenceContext>(
                 opt => opt.UseNpgsql(options.DatabaseConnectionString));
@@ -207,7 +209,6 @@ namespace EvidenceApi
                 }
             });
 
-            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
             app.UseSwagger();
             app.UseRouting();
