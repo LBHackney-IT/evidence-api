6786b0af67c2376575f80fcc4cdddafe26baab27
Charli Wild <charli.wild@charlis-mbp.lan>
2023-01-30 10:00:18 +0000

    enable legacy timestamp behaviour

--- EvidenceApi/Migrations/20230130095730_EnableLegacyTimestamp.Designer.cs
+++ EvidenceApi/Migrations/20230130095730_EnableLegacyTimestamp.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using EvidenceApi.V1.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EvidenceApi.Migrations
 {
     [DbContext(typeof(EvidenceContext))]
-    partial class EvidenceContextModelSnapshot : ModelSnapshot
+    [Migration("20230130095730_EnableLegacyTimestamp")]
+    partial class EnableLegacyTimestamp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EvidenceApi/Startup.cs
+++ EvidenceApi/Startup.cs
@@ -137,6 +137,8 @@ namespace EvidenceApi
             var options = AppOptions.FromEnv();
             services.AddSingleton(x => options);
 
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
             // Database Context
             services.AddDbContext<EvidenceContext>(
                 opt => opt.UseNpgsql(options.DatabaseConnectionString));
@@ -205,8 +207,6 @@ namespace EvidenceApi
                         $"{ApiName}-api {apiVersionDescription.GetFormattedApiVersion()}");
                 }
             });
-
-            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
             app.UseSwagger();
             app.UseRouting();
